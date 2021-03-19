e2eebdb9b69eaf4e155319d96facc3d05d6c50bd
Kyle <kjfossman@gmail.com>
2021-03-19 06:30:15 -0800

    commit tree 49bf6aae87a275307ea54b847329f9911a37d054

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,6 +8,50 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
 
-  get '/' do
+  get '/articles' do 
+    erb :index
   end
+
+  get '/articles/new' do
+    erb :new
+  end
+
+  get '/articles/:id' do 
+    @article = Article.find_by_id(params[:id])
+    erb :show
+  end
+
+  post '/articles' do
+    article = Article.new(params)
+   
+    if article.save 
+      redirect "/articles/#{article.id}"
+    else
+      "Error"
+    end
+  end
+
+  get '/articles/:id/edit' do
+    @article = Article.find_by_id(params[:id])
+    erb :edit
+  end
+
+  patch '/articles/:id' do
+    @article = Article.find_by_id(params[:id])
+    @article.update(title: params[:title], content: params[:content])
+
+    if @article.save
+      redirect "/articles/#{@article.id}"
+    else
+      redirect "articles/#{@article.id}/edit"
+    end
+  end
+
+  delete '/articles/:id' do
+      @article = Article.find_by_id(params[:id])
+      @article.destroy
+      redirect "/articles"
+  end
+   
+    
 end
